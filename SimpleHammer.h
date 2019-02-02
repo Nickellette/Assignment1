bfd8a37754ca6086f4870ce4053ae07a6d6678d6
Nicolette <nicoletteh15e@gmail.com>
2019-02-02 16:32:14 -0500

    Header file for Simple Hammer Weapon

--- SimpleHammer.h
+++ SimpleHammer.h
@@ -1,8 +1,6 @@
 /* 
  * File:   SimpleHammer.h
  * 
- *
- *
  */
 
 #include <string>
@@ -11,10 +9,16 @@
 #ifndef SIMPLEHAMMER_H
 #define SIMPLEHAMMER_H
 
+/*
+ * defines simple hammer 
+ * hitpoint 25
+ * ignores all armor if less than 30
+ */
+
 class SimpleHammer : public Weapon {
 
 	public:
-		SimpleHammer() : Weapon("Simple hammer", 25){}
+		SimpleHammer() : Weapon("Simple hammer", 25.0){}
 		virtual ~SimpleHammer(){};
 		virtual double hit(double armor);
 
