12021cfe2d774a3b3612d16c432ba3ec8f85a830
Nicolette <nicoletteh15e@gmail.com>
2019-02-02 12:05:20 -0500

    Implementation of all weapons in factory

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -9,6 +9,9 @@
 #include "WeaponFactory.h"
 #include "CommonSword.h" 
 #include "CommonSpear.h"
+#include "SimpleHammer.h"
+#include "RandomCrazySword.h"
+#include "PocketKnife.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
 
@@ -26,6 +29,15 @@ Weapon * WeaponFactory::getWeapon(std::string name) {
     
     if (name.compare("spear") == 0) {
         return new CommonSpear();
+    }
+    if (name.compare("hammer") == 0) {
+        return new SimpleHammer();
+    }
+    if (name.compare("random sword") == 0) {
+        return new CrazyRandomSword();
+    }
+    if (name.compare("knife") == 0) {
+        return new PocketKnife();
     }
 
     throw "Invalid weapon";
