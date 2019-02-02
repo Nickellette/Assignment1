e6cc2ee69b33c5d30e30517e757371ccccc2a1d4
Nicolette <nicoletteh15e@gmail.com>
2019-02-02 16:35:01 -0500

    Header file for Crazy Random Sword weapon

--- CrazyRandomSword.h
+++ CrazyRandomSword.h
@@ -5,14 +5,25 @@
 
 #include <string>
 #include "Weapon.h"
+#include <cstdlib>
+#include <ctime>
 
 #ifndef CRAZYRANDOMSWORD_H
 #define CRAZYRANDOMSWORD_H 
 
+/*
+ * defines behavior of a crazy random sword 
+ * RANDOM hitpoint 
+ * RANDOM ignore of armor
+ */
+
 class CrazyRandomSword: public Weapon{
 
 	public:
-	CrazyRandomSword() : Weapon( "Crazy Random Sword", )
+	CrazyRandomSword() : Weapon( "Crazy Random Sword", ((rand()%100+1-7)+7))
+	{
+	
+	}
 	virtual ~CrazyRandomSword() {};
 	virtual double hit(double armor);
 
