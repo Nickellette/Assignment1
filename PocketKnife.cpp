c86656556740ee5936c89ca4ed425807628e756a
Nicolette <nicoletteh15e@gmail.com>
2019-02-02 16:37:45 -0500

    Implementation of Pocket Knife Weapon

--- PocketKnife.cpp
+++ PocketKnife.cpp
@@ -6,7 +6,7 @@
 #include "PocketKnife.h"
 
 double PocketKnife::hit(double armor){
-	double damage = hitPoints - armor
+	double damage = hitPoints - (armor * 0.3);
 	if(damage < 0)
 		return 0;
 	return damage; 
